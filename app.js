4ae00f2c0d76e3f30ae8d44d29f274591d5786a5
rawshad <rawshad@outlook.com>
2021-02-08 11:20:06 +0600

    display country and capital name togather

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+//Display only Country Name
+// fetch("https://restcountries.eu/rest/v2/all")
+// .then(res => res.json())
+// .then(data => displayCountries(data))
+
+// const displayCountries = countries =>{
+//     const ul = document.getElementById("countries");
+//     for (let i = 0; i < countries.length; i++) {
+//         const country = countries[i];
+//         const li = document.createElement("li");
+//         li.innerText = country.name;
+//         ul.appendChild(li);
+//     }
+// }
+//Display Country and Capital
 fetch("https://restcountries.eu/rest/v2/all")
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => displayCountries(data))
+
+const displayCountries = countries =>{
+    const countriesDiv = document.getElementById("countries");
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+        const countryDiv = document.createElement("div");
+        const h3 = document.createElement("h3");
+        h3.innerText = country.name;
+        const p = document.createElement("p");
+        p.innerText = country.capital;
+        countryDiv.appendChild(h3);
+        countryDiv.appendChild(p);
+        countriesDiv.appendChild(countryDiv);
+    }
+}
